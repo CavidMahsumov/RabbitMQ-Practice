ebaacec3e21612be1712d88a36cb95dac7bf4e98
Cavid <76058501+CavidMahsumov@users.noreply.github.com>
2024-02-05 21:45:18 +0400

    Add files via upload

---
+++ PublishLog/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory=new ConnectionFactory();
+factory.HostName="localhost";
+
+var connection=factory.CreateConnection();
+
+var chanel=connection.CreateModel();
+
+chanel.ExchangeDeclare(exchange:"logs",type:ExchangeType.Fanout);
+
+var message=getMessage(args);
+
+var body=Encoding.UTF8.GetBytes(message);
+
+chanel.BasicPublish(exchange:"logs",routingKey:string.Empty,basicProperties:null,body:body);
+
+Console.WriteLine("Mesaj Gonderildi : "+message);
+
+static string getMessage(string[]args){
+    return args.Length>0? string.Join(" ",args):"info : Hello World";
+}
---
+++ ReceiveLogs/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory=new ConnectionFactory();
+factory.HostName="localhost";
+var connection=factory.CreateConnection();
+
+var chanel=connection.CreateModel();
+
+var queueName=chanel.QueueDeclare().QueueName;
+
+chanel.QueueBind(queue:queueName,exchange:"logs",routingKey:string.Empty);
+
+var consumer=new EventingBasicConsumer(chanel);
+
+consumer.Received+=(model,ea)=>{
+    var body=ea.Body.ToArray();
+    var message=Encoding.UTF8.GetString(body);
+    Console.WriteLine("Read the message : "+message);
+};
+
+
+chanel.BasicConsume(queue:queueName,autoAck:false,consumer:consumer);
+
+
+Console.ReadKey();
